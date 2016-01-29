ea31f7ff7fe02d3d290b017f3748e0c34760a67f
Niclas Magnusson <niclas.magnusson@ub.gu.se>
2016-01-29 11:56:13 +0100

    Added root path

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ Router.map(function() {
   this.resource('login');
   this.route('card', {path: '/card/:id'});
   this.route('list');
-  this.route('home');
+  this.route('home', {path: '/'});
   this.route('users', function() {
     this.route('new');
   });
