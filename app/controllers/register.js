2009c7064f43492a105277b56ae958a2f706a143
Magnus Kull <magnus.kull@ub.gu.se>
2016-01-29 15:13:14 +0100

    KAT-19 validation rules for main card

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -18,13 +18,51 @@ export default Ember.Controller.extend({
   showReferenceCard: Ember.computed.equal('cardType', 'reference'),
   showPseudonymCard: Ember.computed.equal('cardType', 'pseudonym'),
 
-
-
   isLookupFieldTypeAuthor: Ember.computed.equal('lookupFieldType', 'author'),
 
   imageUrl: Ember.computed('model', function() {
     return 'http://ipac.ub.gu.se/katalog1957/PictureLoader?Antialias=ON&ImageId=' + this.get('model.ipac_image_id') + '&Scale=1';
   }),
+
+  // Validation properties
+
+  isSignumValid: Ember.computed.notEmpty('signum'),
+
+  isLookupFieldValueValid: Ember.computed.notEmpty('lookupFieldValue'),
+
+  isLookupFieldTypeValid: Ember.computed.notEmpty('lookupFieldType'),
+
+  isTitleValid: Ember.computed('cardType', 'title', function() {
+    if (this.get('cardType') === 'main' && !this.get('title.length')) {
+      return false;
+    } else {
+      return true;
+    }
+  }),
+
+  isYear1Valid: Ember.computed.match('year1', /^\d{4}$/),
+  isYear2Valid: Ember.computed.match('year2', /^\d{4}$/),
+
+  areYearsValid: Ember.computed('year1', 'year2', 'isYear1Valid', 'isYear2Valid', 'isWithoutYear', function() {
+
+    if (this.get('isWithoutYear')) {
+      return true;
+    } else {
+      if (!this.get('year1.length')) {
+        return false;
+      }
+      if (!this.get('isYear1Valid')) {
+        return false;
+      }
+      if (this.get('year2.length') && !this.get('isYear2Valid')) {
+        return false;
+      }
+      return true;
+    }
+  }),
+
+  //isFormComplete
+
 
   actions: {
     addAuthor: function() {
