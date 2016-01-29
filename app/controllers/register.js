f3a2a5629c8c85a36a957cc3039b3f0313a0991a
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-01-29 15:06:04 +0100

    KAT-31 Fix field names to correspond with backend when saving a card, and add save method for primary registration route

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -32,9 +32,8 @@ export default Ember.Controller.extend({
       this.get('authors').pushObject(Ember.Object.create({author: ""}));
 
     },
-    next: function(id) {
-      id++;
-      this.transitionToRoute('card', id);
+    next: function() {
+      this.transitionToRoute('register');
     },
     cancel: function() {
       this.transitionToRoute('home');
