fd30adb6a80fbc4371771d554ef4b8f6b1157645
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-02-02 09:21:20 +0100

    Remove spinner with display: none a second after it has been faded out to prevent cpu usage

--- app/mixins/indicates-loading.js
+++ app/mixins/indicates-loading.js
@@ -3,12 +3,16 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
 
   beforeModel: function(transition) {
+    Ember.$('body').removeClass('loading-remove');
     Ember.$('body').addClass('loading');
     return this._super(transition);
   },
 
   afterModel: function(resolvedModel, transition) {
     Ember.$('body').removeClass('loading');
+    Ember.run.later(this, function() {
+      Ember.$('body').addClass('loading-remove');
+    }, 1000);
     return this._super(resolvedModel, transition);
   }
 });
