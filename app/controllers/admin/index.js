aacddb7c5c6b9915e4d39c6bf07e6d2317545509
Magnus Kull <magnus.kull@ub.gu.se>
2016-02-02 17:49:14 +0100

    observers on admin filters

--- app/controllers/admin/index.js
+++ app/controllers/admin/index.js
@@ -2,8 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ["page", "problem", "image_id"],
-  problem: "all_problems",
+  problem: null,
   image_id: "",
-  page: 1
-});
+  page: 1,
 
+  observesFilter: Ember.observer('problem', 'image_id', function() {
+    this.set('page', 1);
+  }),
+  observesImageId: Ember.observer('image_id', function() {
+    if (this.get('image_id')) {
+      this.set('problem', null);
+    }
+  }),
+  observerProblemFilter: Ember.observer('problem', function() {
+    this.set('image_id', '');
+  })
+});
