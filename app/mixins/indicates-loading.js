4008653f0756e6d61cbd3e0fc4c37f30841c5b35
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-02-02 09:03:31 +0100

    IndicatesLoading needs to be loaded on the home and login routes to remove the loading spinner, and it also needs to call super with proper arguments to pass along the right data to the authentication system

--- app/mixins/indicates-loading.js
+++ app/mixins/indicates-loading.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
-  beforeModel: function() {
+  beforeModel: function(transition) {
     Ember.$('body').addClass('loading');
-    return this._super();
+    return this._super(transition);
   },
 
-  afterModel: function() {
+  afterModel: function(resolvedModel, transition) {
     Ember.$('body').removeClass('loading');
-    return this._super();
+    return this._super(resolvedModel, transition);
   }
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import IndicatesLoading from 'catindex/mixins/indicates-loading';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, IndicatesLoading, {
   model: function() {
     return this.store.find("user", "" + this.get('session.content.secure.username') + "/statistics");
   }
