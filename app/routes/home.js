241086ea92d41fe5169bce2a5213e0ce3a25d48d
Magnus Kull <magnus.kull@ub.gu.se>
2016-02-05 15:54:38 +0100

    resets scroll on home route

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import ResetsScroll from 'catindex/mixins/resets-scroll';
 import IndicatesLoading from 'catindex/mixins/indicates-loading';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, IndicatesLoading, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, ResetsScroll, IndicatesLoading, {
   model: function() {
     return this.store.find("user", "" + this.get('session.content.secure.username') + "/statistics");
   }
