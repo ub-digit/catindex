bf45f027b848880fd6abb35c5613bad43ef32369
Magnus Kull <magnus.kull@ub.gu.se>
2016-02-05 08:36:44 +0100

    KAT-237 stats for admin in home route

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  reviewDisabled: Ember.computed.not("model.statistics.available_for_secondary_registration_count")
+  reviewDisabled: Ember.computed.not("model.statistics.available_for_secondary_registration_count"),
+
+  indexedCards: Ember.computed('model.statistics.totals.card_count', 'model.statistics.totals.not_started_card_count', function() {
+    return this.get('model.statistics.totals.card_count') - this.get('model.statistics.totals.not_started_card_count');
+  }),
+
+  progressbarWidthStyle: Ember.computed('model.statistics.totals.card_count', 'indexedCards', function() {
+    var perc = this.get('indexedCards') / this.get('model.statistics.totals.card_count') * 100;
+    return 'width: ' + perc + '%;';
+  })
+
 });
